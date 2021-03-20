a513a652ff822edce65ebc10b70109626a4ae2d2
Yuri Mello <contato@yurimello.com>
2021-03-20 15:03:34 -0300

    feat: make upsertPlayer controller based in dto create player

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePlayerDto } from './dtos/create_player.dto';
 @Controller('players')
-export class PlayersController {}
+export class PlayersController {
+  @Post()
+  async upsertPlayer(@Body() createPlayerDto: CreatePlayerDto) {
+    const { email, name, phoneNumber } = createPlayerDto;
+
+    return JSON.stringify(`{
+      "name": ${name},
+      "email": ${email},
+      "phoneNumber": ${phoneNumber},
+    }`);
+  }
+}
--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { PlayersController } from './players.controller';
-
 @Module({
   controllers: [PlayersController],
 })
