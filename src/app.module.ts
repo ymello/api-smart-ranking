818e5814203ce6e274150abff244ed7c5f365c05
Yuri Mello <contato@yurimello.com>
2021-04-26 19:19:33 -0300

    connect database in application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    PlayersModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.NODE_USER}:${process.env.NODE_PASS}@cluster1.bfhek.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+  ],
   controllers: [],
   providers: [],
 })
